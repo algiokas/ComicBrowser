86ab8d427ef20d3a51ae59915740d1007856a2c7
Alex Giokas <agiokas1@gmail.com>
2025-06-12 17:10:55 -0500

    functional videosApp component

--- client/src/components/videos/videosApp.tsx
+++ client/src/components/videos/videosApp.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import Navigation from "../shared/navigation";
 import MultiView from "./multiView";
 import { VideosViewMode } from "../../util/enums";
@@ -29,31 +29,36 @@ export interface VideosAppState {
   loadingModalText: string
 }
 
-class VideosApp extends Component<VideosAppProps, VideosAppState> {
-  constructor(props: VideosAppProps) {
-    super(props);
+const getEmptyQuery = (): IVideoSearchQuery => {
+  return {
+    filled: false,
+    actor: '',
+    source: '',
+    tag: '',
+  }
+}
 
-    this.state = {
-      galleryPageSize: 16,
-      allVideos: [],
-      allActors: [],
-      allSources: [],
-      viewMode: VideosViewMode.Loading,
-      currentVideo: null,
-      currentSearchQuery: this.getEmptyQuery(),
-      showLoadingModal: false,
-      loadingModalText: ""
+function VideosApp(props: VideosAppProps) {
+  const [allVideos, setAllVideos] = useState<IVideo[]>([])
+  const [allActors, setAllActors] = useState<IActor[]>([])
+  const [allSources, setAllSources] = useState<IVideoSource[]>([])
+  const [viewMode, setViewMode] = useState<VideosViewMode>(VideosViewMode.Loading)
+  const [currentVideo, setCurrentVideo] = useState<IVideo | null>(null)
+  const [currentSearchQuery, setCurrentSearchQuery] = useState<IVideoSearchQuery>(getEmptyQuery())
+  const [showLoadingModal, setShowLoadingModal] = useState<boolean>(false)
+  const [loadingModalText, setLoadingModalText] = useState<string>("")
+  const [galleryPageSize, setGalleryPageSize] = useState<number>(16)
+
+  useEffect(() => {
+    const init = async () => {
+      await fillData()
+      setViewMode(VideosViewMode.Listing)
     }
-  }
+    init();
+  }, [])
 
-  componentDidMount() {
-    this.fillData();
-    this.setState({
-      viewMode: VideosViewMode.Listing
-    })
-  }
 
-  videosFromJson(videoJson: any): IVideo[] {
+  const videosFromJson = (videoJson: any): IVideo[] => {
     let videoList: IVideo[] = []
     if (!videoJson || videoJson.length < 1) console.log("videosFromJson - no videos in input")
     videoJson.forEach((e: any): any => {
@@ -66,166 +71,133 @@ class VideosApp extends Component<VideosAppProps, VideosAppState> {
     return videoList
   }
 
-  actorsFromJson(actorJson: any, videoJson: any): IActor[] {
+  const actorsFromJson = (actorJson: any, videoJson: any): IActor[] => {
     let actorList: IActor[] = []
     if (!actorJson || actorJson.length < 1) console.log("actorsFromJson - no actors in input")
     actorJson.forEach((e: any): any => {
       let newActor = e as IActor
       newActor.isFavorite = e.isFavorite != null && e.isFavorite > 0
-      newActor.videos = videoJson.filter((v : any) => v.actors.some((a : any) => a.id === newActor.id)).map((v : any) => v.id)
+      newActor.videos = videoJson.filter((v: any) => v.actors.some((a: any) => a.id === newActor.id)).map((v: any) => v.id)
       actorList.push(newActor)
     });
     return actorList
   }
 
-  fillData = () => {
-    fetch(`${apiBaseUrl}/videos`)
-      .then(res => res.json())
-      .then(data => {
-        console.log('fetch videos')
-        var videoJson = this.videosFromJson(data)
-        this.setState({ allVideos: videoJson })
-        this.fillActors(videoJson)
-      });
+  const fillData = async () => {
+    const res = await fetch(`${apiBaseUrl}/videos`)
+    const data = await res.json()
+    const videos = videosFromJson(data)
+    await setAllVideos(videos)
+    await fillActors(videos)
   }
 
-  fillActors = (videoJson: any) => {
-    fetch(`${apiBaseUrl}/actors`)
-      .then(res => res.json())
-      .then(data => {
-        console.log('fetch actors')
-        var actorJson = this.actorsFromJson(data, videoJson)
-        this.setState({ allActors: actorJson })
-      });
+  const fillActors = async (videoJson: any) => {
+    const res = await fetch(`${apiBaseUrl}/actors`)
+    const data = await res.json()
+    const actors = actorsFromJson(data, videoJson)
+    setAllActors(actors)
   }
 
-  fillSources = () => {
-    fetch(`${apiBaseUrl}/videos/sources`)
-      .then(res => res.json())
-      .then(data => {
-        console.log('fetch sources')
-        let sources = data.map((s: any) => s as IVideoSource)
-        this.setState({ allSources: sources })
-      });
+  const fillSources = async () => {
+    const res = await fetch(`${apiBaseUrl}/videos/sources`)
+    const data = await res.json()
+    const sources = data.map((s: any) => s as IVideoSource)
+    setAllSources(sources)
   }
 
-  updateVideo = (video: IVideo) => {
-    fetch(`${apiBaseUrl}/videos/${video.id}/update`, {
+  const updateVideo = async (video: IVideo) => {
+    const res = await fetch(`${apiBaseUrl}/videos/${video.id}/update`, {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(video)
     })
-      .then(res => res.json())
-      .then(data => {
-        if (data.success) {
-          console.log("Video " + video.id + " Updated")
-          for (let i = 0; i < this.state.allVideos.length; i++) {
-            if (this.state.allVideos[i] && this.state.allVideos[i].id === data.video.id) {
-              this.setState((state) => {
-                let videos = state.allVideos
-                videos[i] = data.video
-                return { allVideos: videos }
-              })
-            }
-          }
+    const data = await res.json()
+    if (data.success) {
+      console.log("Video " + video.id + " Updated")
+      for (let i = 0; i < allVideos.length; i++) {
+        if (allVideos[i] && allVideos[i].id === data.video.id) {
+          const videos = allVideos
+          videos[i] = data.video
+          setAllVideos(videos)
         }
-      });
+      }
+    }
   }
 
-  updateActor = (actor: IActor) => {
-    fetch(`${apiBaseUrl}/actors/${actor.id}/update`, {
+  const updateActor = async (actor: IActor) => {
+    const res = await fetch(`${apiBaseUrl}/actors/${actor.id}/update`, {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(actor)
     })
-      .then(res => res.json())
-      .then(data => {
-        if (data.success) {
-          console.log("Actor " + actor.id + " Updated")
-          for (let i = 0; i < this.state.allActors.length; i++) {
-            if (this.state.allActors[i] && this.state.allActors[i].id === data.actor.id) {
-              this.setState((state) => {
-                let actors = state.allActors
-                let newActor = data.actor as IActor
-                newActor.isFavorite = data.actor.isFavorite != null && data.actor.isFavorite > 0
-                newActor.videos = this.state.allVideos.filter((v : any) => v.actors.some((a : any) => a.id === newActor.id)).map((v : any) => v.id)
-                actors[i] = newActor
-                return { allActors: actors }
-              })
-            }
-          }
+    const data = await res.json()
+    if (data.success) {
+      console.log("Actor " + actor.id + " Updated")
+      for (let i = 0; i < allActors.length; i++) {
+        if (allActors[i] && allActors[i].id === data.actor.id) {
+          let actors = allActors
+          let newActor = data.actor as IActor
+          newActor.isFavorite = data.actor.isFavorite != null && data.actor.isFavorite > 0
+          newActor.videos = allVideos.filter((v: any) => v.actors.some((a: any) => a.id === newActor.id)).map((v: any) => v.id)
+          actors[i] = newActor
+          setAllActors(actors)
         }
-      });
+      }
+    }
   }
 
-  deleteVideo = (videoId: number) => {
+  const deleteVideo = async (videoId: number) => {
     console.log('delete video with id: ' + videoId)
-    fetch(`${apiBaseUrl}/videos/${videoId}`, {
+    const res = await fetch(`${apiBaseUrl}/videos/${videoId}`, {
       method: 'delete'
     })
-      .then(res => res.json())
-      .then(data => {
-        if (data.changes > 0) {
-          console.log('removed video ID: ' + videoId)
-          this.setState((state: VideosAppState): object | null => {
-            return ({
-              allVideos: state.allVideos.filter((b) => b.id !== videoId),
-              currentVideo: {},
-              viewMode: VideosViewMode.Listing
-            })
-          })
-        }
-      });
+    const data = await res.json()
+    if (data.changes > 0) {
+      console.log('removed video ID: ' + videoId)
+      setAllVideos(allVideos.filter((b) => b.id !== videoId))
+      setCurrentVideo(null)
+      setViewMode(VideosViewMode.Listing)
+    }
   }
 
-  importVideos = () => {
+  const importVideos = async () => {
     console.log("Importing Videos")
-    this.setState({ viewMode: VideosViewMode.Loading })
-    fetch(apiBaseUrl + 'videos/import', {
+    setViewMode(VideosViewMode.Loading)
+    const res = await fetch(apiBaseUrl + 'videos/import', {
       method: 'get',
       headers: { 'Content-Type': 'application/json' }
     })
-      .then(res => res.json())
-      .then(data => {
-        if (data) {
-          this.setState({
-            allVideos: this.videosFromJson(data.videos),
-            currentVideo: null,
-            viewMode: VideosViewMode.Listing
-          })
-        }
-      });
+    const data = await res.json()
+    if (data) {
+      setAllVideos(videosFromJson(data.videos))
+      setCurrentVideo(null)
+      setViewMode(VideosViewMode.Listing)
+    }
   }
 
-  setThumbnailToTime = (videoId: number, timeMs: number) => {
+  const setThumbnailToTime = async (videoId: number, timeMs: number) => {
     console.log('set thumbnail for video:' + videoId + ' to ' + timeMs + 'ms')
-    this.toggleLoadingModal("Generating thumbnail for video " + videoId)
-    fetch(apiBaseUrl + '/videos/thumbnail/' + videoId + '/generate/' + timeMs, {
+    toggleLoadingModal("Generating thumbnail for video " + videoId)
+    const res = await fetch(apiBaseUrl + '/videos/thumbnail/' + videoId + '/generate/' + timeMs, {
       method: 'post'
     })
-      .then(res => res.json())
-      .then(data => {
-        if (data.success) {
-          console.log('successfully updated thumbnail for: ' + videoId + " new thumb: " + data.video.thumbnailId)
-          for (let i = 0; i < this.state.allVideos.length; i++) {
-            if (this.state.allVideos[i] && this.state.allVideos[i].id === data.video.id) {
-              this.setState((state) => {
-                let videos = state.allVideos
-                videos[i].thumbnailId = data.video.thumbnailId
-                return { 
-                  allVideos: videos,
-                  showLoadingModal: false
-                }
-              })
-            }
-          }
+    const data = await res.json()
+    if (data.success) {
+      console.log('successfully updated thumbnail for: ' + videoId + " new thumb: " + data.video.thumbnailId)
+      for (let i = 0; i < allVideos.length; i++) {
+        if (allVideos[i] && allVideos[i].id === data.video.id) {
+          let videos = allVideos
+          videos[i].thumbnailId = data.video.thumbnailId
+          setAllVideos(videos)
+          setShowLoadingModal(false)
         }
-      });
+      }
+    }
   }
 
-  generateImageForActor = (videoId: number, actorId: number, timeMs: number) => {
+  const generateImageForActor = async (videoId: number, actorId: number, timeMs: number) => {
     console.log('Generating image for actor: ' + actorId + " from video " + videoId + " @" + timeMs + 'ms')
-    this.toggleLoadingModal("Generating image for actor " + actorId)
-    fetch(apiBaseUrl + 'actors/' + actorId + '/imagefromvideo', {
+    toggleLoadingModal("Generating image for actor " + actorId)
+    const res = await fetch(apiBaseUrl + 'actors/' + actorId + '/imagefromvideo', {
       method: 'post',
       headers: {
         'Accept': 'application/json, text/plain, */*',
@@ -236,171 +208,152 @@ class VideosApp extends Component<VideosAppProps, VideosAppState> {
         timeMs: timeMs
       })
     })
-      .then(res => res.json())
-      .then(data => {
-        if (data.success) {
-          console.log('successfully generated  image for: ' + actorId + " new image: " + data.actor.imageFile)
-          for (let i = 0; i < this.state.allActors.length; i++) {
-            if (this.state.allActors[i] && this.state.allActors[i].id === data.actor.id) {
-              this.setState((state) => {
-                let actors = state.allActors
-                actors[i].imageFile = data.actor.imageFile
-                return { 
-                  allActors: actors,
-                  showLoadingModal: false
-                }
-              })
-            }
-          }
+    const data = await res.json()
+    if (data.success) {
+      console.log('successfully generated  image for: ' + actorId + " new image: " + data.actor.imageFile)
+      for (let i = 0; i < allActors.length; i++) {
+        if (allActors[i] && allActors[i].id === data.actor.id) {
+          let actors = allActors
+          actors[i].imageFile = data.actor.imageFile
+          setAllActors(actors)
+          setShowLoadingModal(false)
         }
-      });
-  }
-
-  watchVideo = (video: IVideo) => {
-    this.setState({
-      viewMode: VideosViewMode.Player,
-      currentVideo: video
-    })
-  }
-
-  viewCurrentVideo = () => {
-    if (this.state.currentVideo && this.state.currentVideo.title) {
-      this.setState({
-        viewMode: VideosViewMode.Player
-      })
+      }
     }
   }
 
-  getEmptyQuery(): IVideoSearchQuery {
-    return {
-      filled: false,
-      actor: '',
-      source: '',
-      tag: '',
+  const watchVideo = (video: IVideo) => {
+    setCurrentVideo(video)
+    setViewMode(VideosViewMode.Player)
+  }
+
+  const viewCurrentVideo = () => {
+    if (currentVideo && currentVideo.title) {
+      setViewMode(VideosViewMode.Player)
     }
   }
 
-  viewSearchResults = (query?: IVideoSearchQuery) => {
+
+
+  const viewSearchResults = (query?: IVideoSearchQuery) => {
     if (query) {
-      let newQuery = { ...this.getEmptyQuery(), ...query }
+      let newQuery = { ...getEmptyQuery(), ...query }
       newQuery.filled = true
-      this.setState({
-        currentSearchQuery: newQuery,
-        viewMode: VideosViewMode.SearchResults
-      })
-    } else if (this.state.currentSearchQuery.filled) {
-      this.setState({
-        viewMode: VideosViewMode.SearchResults
-      })
+      setCurrentSearchQuery(newQuery)
+      setViewMode(VideosViewMode.SearchResults)
+    } else if (currentSearchQuery.filled) {
+      setViewMode(VideosViewMode.SearchResults)
     }
   }
 
-  viewListing = () => {
-    this.setState({
-      viewMode: VideosViewMode.Listing
-    })
+  const viewListing = () => {
+    setViewMode(VideosViewMode.Listing)
   }
 
-  viewActors = () => {
-    this.setState({
-      viewMode: VideosViewMode.Actors
-    })
+  const viewActors = () => {
+    setViewMode(VideosViewMode.Actors)
   }
 
-  getActorImageUrlWithFallback = (actor: IActor): string => {
-    let matchingActor = this.state.allActors.find((a) => a.id === actor.id)
+  const getActorImageUrlWithFallback = (actor: IActor): string => {
+    let matchingActor = allActors.find((a) => a.id === actor.id)
     if (!matchingActor) return ""
     if (matchingActor.imageFile) return getActorImageUrl(actor)
-    let matchingVideos = this.state.allVideos.filter((v) => v.actors.some((a) => a.id === actor.id) && v.thumbnailId)
+    let matchingVideos = allVideos.filter((v) => v.actors.some((a) => a.id === actor.id) && v.thumbnailId)
     if (matchingVideos.length > 0) return getVideoThumbnailUrl(matchingVideos[0])
     return ""
   }
 
-  getLeftNavItems(): INavItem[] {
+  const getLeftNavItems = (): INavItem[] => {
     return [
       {
         text: "Videos",
         viewMode: VideosViewMode.Listing,
-        clickHandler: this.viewListing
+        clickHandler: viewListing
       },
       {
         text: "Actors",
         viewMode: VideosViewMode.Actors,
-        clickHandler: this.viewActors
+        clickHandler: viewActors
       },
       {
         text: "Current Video",
         viewMode: VideosViewMode.Player,
-        clickHandler: this.viewCurrentVideo
+        clickHandler: viewCurrentVideo
       },
       {
         text: "Search Results",
         viewMode: VideosViewMode.SearchResults,
-        clickHandler: this.viewSearchResults
+        clickHandler: viewSearchResults
       }
     ]
   }
 
-  getRightNavItems(): INavItem[] {
+  const getRightNavItems = (): INavItem[] => {
     return [
       {
         text: "Import Videos",
-        clickHandler: this.importVideos
+        clickHandler: importVideos
       },
       {
         text: "Books",
-        clickHandler: this.props.toggleAppMode
+        clickHandler: props.toggleAppMode
       }]
   }
 
-  toggleLoadingModal = (text?: string) => {
-    this.setState((state) => {
-      return {
-        showLoadingModal: !state.showLoadingModal,
-        loadingModalText: text ?? ""
-      }
-    })
+  const toggleLoadingModal = (text?: string) => {
+    setLoadingModalText(text ?? "")
+    setShowLoadingModal(!showLoadingModal)
   }
 
-  render() {
-    const navProps = {
-      viewMode: this.state.viewMode,
-      leftNavItems: this.getLeftNavItems(),
-      rightNavItems: this.getRightNavItems(),
-      showSearch: true,
-      logoClick: this.viewListing,
-      viewSearchResults: this.viewSearchResults
-    }
-
-    const handlers = {
-      watchVideo: this.watchVideo,
-      viewListing: this.viewListing,
-      viewActors: this.viewActors,
-      viewCurrentVideo: this.viewCurrentVideo,
-      viewSearchResults: this.viewSearchResults,
-      updateVideo: this.updateVideo,
-      deleteVideo: this.deleteVideo,
-      importVideos: this.importVideos,
-      setThumbnailToTime: this.setThumbnailToTime,
-      updateActor: this.updateActor,
-      getActorImageUrl: this.getActorImageUrlWithFallback,
-      generateImageForActor: this.generateImageForActor
-    }
-
-    return (
-      <div className="VideosApp">
-        <Modal 
-          modalId="loading-modal"
-          displayModal={this.state.showLoadingModal}
-          toggleModal={this.toggleLoadingModal}>
-          <span className="loading-modal-text">{this.state.loadingModalText}</span>
-        </Modal>
-        <Navigation {...navProps}>
-        </Navigation>
-        <MultiView {...this.state} {...handlers}></MultiView>
-      </div>
-    )
+  const navProps = {
+    viewMode: viewMode,
+    leftNavItems: getLeftNavItems(),
+    rightNavItems: getRightNavItems(),
+    showSearch: true,
+    logoClick: viewListing,
+    viewSearchResults: viewSearchResults
   }
+
+  const handlers = {
+    watchVideo: watchVideo,
+    viewListing: viewListing,
+    viewActors: viewActors,
+    viewCurrentVideo: viewCurrentVideo,
+    viewSearchResults: viewSearchResults,
+    updateVideo: updateVideo,
+    deleteVideo: deleteVideo,
+    importVideos: importVideos,
+    setThumbnailToTime: setThumbnailToTime,
+    updateActor: updateActor,
+    getActorImageUrl: getActorImageUrlWithFallback,
+    generateImageForActor: generateImageForActor
+  }
+
+  const appState: VideosAppState = {
+    galleryPageSize: galleryPageSize,
+    allVideos: allVideos,
+    allActors: allActors,
+    allSources: allSources,
+    viewMode: viewMode,
+    currentVideo: currentVideo,
+    currentSearchQuery: currentSearchQuery,
+    showLoadingModal: showLoadingModal,
+    loadingModalText: loadingModalText
+  }
+
+  return (
+    <div className="VideosApp">
+      <Modal
+        modalId="loading-modal"
+        displayModal={showLoadingModal}
+        toggleModal={toggleLoadingModal}>
+        <span className="loading-modal-text">{loadingModalText}</span>
+      </Modal>
+      <Navigation {...navProps}>
+      </Navigation>
+      <MultiView {...appState} {...handlers}></MultiView>
+    </div>
+  )
 }
 
 export default VideosApp;
